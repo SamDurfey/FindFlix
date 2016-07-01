3e80c90a9c0356c6f7794a78b952c5d20881f69c
SamDurfey <samuel.durfey@gmail.com>
2016-07-01 11:59:14 -0700

    Implement a Toast on roulette activity

--- app/src/main/java/com/epicodus/nightatthemovies/RouletteActivity.java
+++ app/src/main/java/com/epicodus/nightatthemovies/RouletteActivity.java
@@ -3,14 +3,19 @@ package com.epicodus.nightatthemovies;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class RouletteActivity extends AppCompatActivity {
+public class RouletteActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.rouletteHeader) TextView mRouletteHeader;
+    @Bind(R.id.randomizeButton) Button mRandomizeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,12 @@ public class RouletteActivity extends AppCompatActivity {
 
         Typeface limelightFont = Typeface.createFromAsset(getAssets(), "fonts/Limelight.ttf");
         mRouletteHeader.setTypeface(limelightFont);
+
+        mRandomizeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(RouletteActivity.this, "You will be able to click this button to be given something random to watch.", Toast.LENGTH_LONG).show();
     }
 }
