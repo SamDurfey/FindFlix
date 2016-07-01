373f71ed037bfb69bd702bd9d4545316bbe30f63
SamDurfey <samuel.durfey@gmail.com>
2016-07-01 08:41:21 -0700

    Initial commit. ButterKnife and custom typeface in place.

---
+++ app/src/main/java/com/epicodus/nightatthemovies/MainActivity.java
@@ -0,0 +1,32 @@
+package com.epicodus.nightatthemovies;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.appTitle) TextView mAppTitle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface limelightFont = Typeface.createFromAsset(getAssets(), "fonts/Limelight.ttf");
+        mAppTitle.setTypeface(limelightFont);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //click stuff
+    }
+}
