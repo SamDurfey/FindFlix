2a3b505905631d3c2918f6c591446a612df5e9f3
SamDurfey <samuel.durfey@gmail.com>
2016-07-15 15:51:30 -0700

    Get past technical difficulties. Successfully authenticates.

--- app/src/main/java/com/epicodus/nightatthemovies/ui/NewAccountActivity.java
+++ app/src/main/java/com/epicodus/nightatthemovies/ui/NewAccountActivity.java
@@ -1,34 +1,104 @@
 package com.epicodus.nightatthemovies.ui;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.epicodus.nightatthemovies.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class NewAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG =  NewAccountActivity.class.getSimpleName() ;
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor mEditor;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Bind(R.id.usernameEditText) EditText mUserNameEntry;
     @Bind(R.id.emailEditText) EditText mEmailEntry;
     @Bind(R.id.passwordEditText) EditText mPasswordEntry;
     @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEntry;
+    @Bind(R.id.signUpButton) Button mSignUpButton;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
         ButterKnife.bind(this);
+
+        mSignUpButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
     }
 
     @Override
     public void onClick(View view) {
+        if (view == mSignUpButton) {
+            createNewUser();
+        }
+    }
 
+    private void createNewUser() {
+        final String name = mUserNameEntry.getText().toString().trim();
+        final String email = mEmailEntry.getText().toString().trim();
+        String password = mPasswordEntry.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEntry.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful.");
+                        } else {
+                            Toast.makeText(NewAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+        });
+    }
+
+    private void createAuthStateListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(NewAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    finish();
+                }
+            }
+        };
     }
 }
