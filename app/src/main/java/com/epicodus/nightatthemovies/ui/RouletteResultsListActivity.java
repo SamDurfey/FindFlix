10d3b0e29807729b63a1472ed90605bb8753cc1b
SamDurfey <samuel.durfey@gmail.com>
2016-07-21 15:36:29 -0700

    Create list activity. Start on recycler view.

---
+++ app/src/main/java/com/epicodus/nightatthemovies/ui/RouletteResultsListActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.nightatthemovies.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.nightatthemovies.R;
+
+public class RouletteResultsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roulette_results_list);
+    }
+}
